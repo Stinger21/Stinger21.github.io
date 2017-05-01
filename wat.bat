5f432dfcd82b9f70bdbd01b4125ab8e9f5cb1baf
Stinger21 <mattias.westphal@gmail.com>
2017-05-01 12:07:58 +0200

    lol, shenanigans.

--- wat.bat
+++
@@ -1,5 +0,0 @@
-git add -A
-git commit -am "lol, shenanigans."
-git push --repo https://Stinger21:password@github.com/Stinger21/Stinger21.github.io.git
-Something Something Dont Check in
-origin master
